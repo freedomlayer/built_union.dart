16d385e781291a615be2875601b8bee6575b9960
real <real@freedomlayer.org>
2020-01-31 14:17:46 +0200

    built_union: Added a custom json plugin (To deal with empty union variants)

---
+++ built_union/lib/custom_json_plugin.dart
@@ -0,0 +1,44 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/json_object.dart';
+import 'package:built_value/standard_json_plugin.dart';
+import 'package:built_value/serializer.dart';
+
+
+class CustomJsonPlugin extends StandardJsonPlugin {
+  @override
+  Object afterSerialize(Object object, FullType specifiedType) {
+    if (object is List &&
+        specifiedType.root != BuiltList &&
+        specifiedType.root != BuiltSet &&
+        specifiedType.root != JsonObject) {
+      if (specifiedType.isUnspecified) {
+        return super.afterSerialize(object, specifiedType);
+      } else {
+        // A case used for an empty Union variant:
+        if (object.length == 1 && object[0] != 'list') {
+          return object[0];
+        }
+        return super.afterSerialize(object, specifiedType);
+      }
+    } else {
+      return object;
+    }
+  }
+
+  @override
+  Object beforeDeserialize(Object object, FullType specifiedType) {
+    if (object is Map && specifiedType.root != JsonObject) {
+      if (specifiedType.isUnspecified) {
+        return super.beforeDeserialize(object, specifiedType);
+      } else {
+        return super.beforeDeserialize(object, specifiedType);
+      }
+    } else {
+      // A case used for an empty Union variant
+      if (specifiedType.root != String && object is String) {
+        return <Object>[object];
+      }
+      return object;
+    }
+  }
+}
