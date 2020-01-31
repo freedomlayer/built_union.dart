27597ddec2f3bf9965029e1ceab8f46772b57b4e
real <real@freedomlayer.org>
2020-01-31 13:32:55 +0200

    built_union_test: Using local standard_json_plugin.dart for serialization

--- built_union_test/test/values_test.dart
+++ built_union_test/test/values_test.dart
@@ -1,6 +1,6 @@
 // import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:built_value/standard_json_plugin.dart';
+import 'package:built_union/standard_json_plugin.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:test/test.dart';
 
